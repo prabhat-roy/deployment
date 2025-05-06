f2c1c46f0d40a49e607b335466056cf02a08fc8b
Prabhat <prabhatkumar.roy@outlook.com>
2025-05-06 21:37:59 +0530

    initial commit

--- groovy/global.groovy
+++ groovy/global.groovy
@@ -2,22 +2,21 @@ def loadGlobalEnv = {
     echo "[INFO] Loading global environment variables..."
 
     def envContent = readFile 'groovy/global.env'
-    def props = [:]
+    def envList = []
 
     envContent.split('\n').each { line ->
         line = line.trim()
         if (line && !line.startsWith("#")) {
             def (key, value) = line.tokenize('=')
             if (key && value) {
-                props[key.trim()] = value.trim()
-                // Also set it to environment dynamically
-                env[key.trim()] = value.trim()
-                echo "[DEBUG] Loaded: ${key.trim()}=${value.trim()}"
+                def entry = "${key.trim()}=${value.trim()}"
+                envList.add(entry)
+                echo "[DEBUG] Parsed: ${entry}"
             }
         }
     }
 
-    echo "[INFO] Global environment variables loaded successfully."
+    return envList
 }
 
 return [loadGlobalEnv: loadGlobalEnv]
