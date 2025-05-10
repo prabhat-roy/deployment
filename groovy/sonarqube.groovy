caee4dec2d617c47e3099ea867d7f70a891d2f06
Prabhat <prabhatkumar.roy@outlook.com>
2025-05-11 03:42:18 +0530

    initial commit

--- groovy/sonarqube.groovy
+++ groovy/sonarqube.groovy
@@ -1,9 +1,18 @@
 def installSonarqube() {
-    // Make sure the shell script is executable
-    sh "chmod +x shell_script/sonarqube.sh"
+    withCredentials([
+        usernamePassword(credentialsId: 'jenkins-cred', usernameVariable: 'JENKINS_USER', passwordVariable: 'JENKINS_PASS')
+    ]) {
+        // Ensure the shell script is executable
+        sh "chmod +x shell_script/sonarqube.sh"
 
-    // Call the shell script to install unzip
-    sh "shell_script/sonarqube.sh"
+        // Run the shell script with env vars passed through
+        sh """
+            export JENKINS_URL="${env.JENKINS_URL}"
+            export JENKINS_USER="${JENKINS_USER}"
+            export JENKINS_PASS="${JENKINS_PASS}"
+            shell_script/sonarqube.sh
+        """
+    }
 }
 
 return this
