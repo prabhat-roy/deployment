6d6904454dfc19816f096bd8f6ced4df7c33c34a
Prabhat <prabhatkumar.roy@outlook.com>
2025-05-11 03:22:43 +0530

    initial commit

--- groovy/terraform.groovy
+++ groovy/terraform.groovy
@@ -1,6 +1,6 @@
 def installTerraform() {
     // Make sure the shell script is executable
-    sh "chmod +x shell_script/terrafom.sh"
+    sh "chmod +x shell_script/terraform.sh"
 
     // Call the shell script to install unzip
     sh "shell_script/terraform.sh"
