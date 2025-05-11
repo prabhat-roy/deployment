76e07695a661379606f71149287d34f4868ca672
Prabhat <prabhatkumar.roy@outlook.com>
2025-05-11 12:23:33 +0530

    initial commit

--- groovy/owasp.groovy
+++ groovy/owasp.groovy
@@ -2,8 +2,8 @@ def installOwasp() {
     // Make sure the shell script is executable
     sh "chmod +x shell_script/owasp.sh"
 
-    // Call the shell script to install unzip
-    sh "shell_script/owasp.sh"
+    // Call the shell script to install uowasp
+    sh "sudo shell_script/owasp.sh"
 }
 
 return this
