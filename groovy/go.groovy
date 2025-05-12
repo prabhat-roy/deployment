199856c442c667ab6c9c7dd2b0bfadf97fb8a241
Prabhat <prabhatkumar.roy@outlook.com>
2025-05-12 20:59:56 +0530

    initial commit

--- groovy/go.groovy
+++
@@ -1,9 +0,0 @@
-def installGo() {
-    // Make sure the shell script is executable
-    sh "chmod +x shell_script/go.sh"
-
-    // Call the shell script to install go
-    sh "shell_script/go.sh"
-}
-
-return this
