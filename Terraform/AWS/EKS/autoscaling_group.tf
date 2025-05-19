8df7e18b7c553812331db8c207e818ff7c4143bd
Prabhat <prabhatkumar.roy@outlook.com>
2025-05-20 02:55:32 +0530

    initial commit

--- Terraform/AWS/EKS/autoscaling_group.tf
+++ Terraform/AWS/EKS/autoscaling_group.tf
@@ -13,4 +13,10 @@ resource "aws_autoscaling_group" "eks_workers" {
   lifecycle {
     create_before_destroy = true
   }
+
+  tag {
+    key                 = "Name"
+    value               = "eks-worker"
+    propagate_at_launch = true
+  }
 }
