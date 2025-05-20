fb8744af8569882f8e5c26f24adfa800bd8957bb
Prabhat <prabhatkumar.roy@outlook.com>
2025-05-21 04:51:25 +0530

    initial commit

--- Terraform/Azure/AKS/provider.tf
+++ Terraform/Azure/AKS/provider.tf
@@ -1,11 +1,18 @@
 terraform {
+  required_version = ">= 1.6.0"
+
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.29.0" # Latest stable as of May 2025
+    }
     null = {
       source  = "hashicorp/null"
-      version = "4.29.0"
+      version = "3.2.4" # Latest stable as of May 2025
     }
   }
 }
+
 
 provider "azurerm" {
   subscription_id = var.subscription_id
